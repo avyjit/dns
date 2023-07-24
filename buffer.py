92a29237ea988205ca104f708dc923cb6f28807a
Avyyukt Ajith <avyyukt.ajith@gmail.com>
2023-07-24 13:18:37 +0530

    scaffold

---
+++ buffer.py
@@ -0,0 +1,107 @@
+import struct
+from dataclasses import dataclass
+
+DNS_QNAME_MAX_JUMPS = 5
+
+
+@dataclass
+class Buffer:
+    buf: bytes = b""
+    offset: int = 0
+
+    def unpack(self, fmt):
+        values = struct.unpack_from(fmt, self.buf, self.offset)
+        self.offset += struct.calcsize(fmt)
+        return values
+
+    def peek(self, fmt):
+        return struct.unpack_from(fmt, self.buf, self.offset)
+
+    def seek(self, offset):
+        self.offset = offset
+
+    def get(self, pos):
+        return self.buf[pos]
+
+    def get_range(self, start: int, length: int):
+        end = start + length
+        assert end <= len(self), "Buffer out of range"
+        assert end <= 512, "Packet length exceeds 512 bytes"
+
+        return self.buf[start:end]
+
+    def pos(self):
+        return self.offset
+
+    def __len__(self):
+        return len(self.buf)
+
+    def read_qname(self):
+        qname = ""
+
+        pos = self.pos()
+        jumped = False
+        num_jumps = 0
+
+        delim = ""
+
+        while True:
+            assert (
+                num_jumps < DNS_QNAME_MAX_JUMPS
+            ), "Too many jumps in DNS qname parsing"
+
+            # Read a single byte indicating length of next segment
+            (length,) = self.peek("!B")
+
+            if length & 0xC0 == 0xC0:
+                if not jumped:
+                    self.seek(self.pos() + 2)
+
+                b2 = self.get(pos + 1)
+                pos = ((length ^ 0xC0) << 8) + b2
+
+                jumped = True
+                num_jumps += 1
+
+                continue
+
+            else:
+                pos += 1
+
+                # The qname is terminated by a zero-length segment
+                if length == 0:
+                    self.seek(pos)
+                    break
+
+                qname += delim
+                qname += self.get_range(pos, length).decode("utf-8").lower()
+
+                delim = "."
+                pos += length
+                self.seek(pos)
+
+            # if not jumped:
+            #     self.seek(pos)
+
+        return qname
+
+    def write_byte(self, byte: int):
+        self.buf += struct.pack("!B", byte)
+        self.offset += 1
+
+    def write(self, fmt: str, *vals):
+        size = struct.calcsize(fmt)
+        self.buf += struct.pack(fmt, *vals)
+        self.offset += size
+
+    def write_qname(self, qname: str):
+        for label in qname.split("."):
+            length = len(label)
+            assert length <= 63, "Label exceeds 63 characters"
+
+            self.write("!B", length)
+            raw_bytes = label.encode("utf-8")
+            self.buf += raw_bytes
+            self.offset += len(raw_bytes)
+
+        self.write("!B", 0)
---
+++ packet.py
@@ -0,0 +1,238 @@
+import ipaddress
+import pprint
+from dataclasses import asdict, dataclass
+from enum import IntEnum
+from typing import List, Union
+
+from buffer import Buffer
+
+
+class ResultCode(IntEnum):
+    NOERROR = 0
+    FORMERR = 1
+    SERVFAIL = 2
+    NXDOMAIN = 3
+    NOTIMP = 4
+    REFUSED = 5
+
+
+class QueryType(IntEnum):
+    UNKNOWN = 0
+    A = 1
+
+
+@dataclass
+class DnsHeader:
+    id: int  # 16 bits
+
+    recursion_desired: bool  # 1 bit
+    truncated_message: bool  # 1 bit
+    authoritative_answer: bool  # 1 bit
+    opcode: int  # 4 bits
+    response: bool  # 1 bit
+
+    rescode: ResultCode  # 4 bits
+    checking_disabled: bool  # 1 bit
+    authed_data: bool  # 1 bit
+    z: bool  # 1 bit
+    recursion_available: bool  # 1 bit
+
+    questions: int  # 16 bits
+    answers: int  # 16 bits
+    authoritative_entries: int  # 16 bits
+    resource_entries: int  # 16 bits
+
+    @classmethod
+    def parse(cls, buf: Buffer):
+        assert len(buf) >= 12, "Packet header must be atleast 12 bytes"
+        (id, flags, questions, answers, auth, res) = buf.unpack("!HHHHHH")
+
+        # a represents the first 8 bits of the flags
+        # b represents the last 8 bits of the flags
+        # flags is 16 bits long
+        a, b = flags >> 8, flags & 0xFF
+
+        recursion_desired = (a & (1 << 0)) > 0
+        truncated_message = (a & (1 << 1)) > 0
+        authoritative_answer = (a & (1 << 2)) > 0
+        opcode = (a >> 3) & 0x0F
+        response = (a & (1 << 7)) > 0
+
+        rescode = ResultCode(b & 0x0F)
+        checking_disabled = (b & (1 << 4)) > 0
+        authed_data = (b & (1 << 5)) > 0
+        z = (b & (1 << 6)) > 0
+        recursion_available = (b & (1 << 7)) > 0
+
+        return cls(
+            id=id,
+            recursion_desired=recursion_desired,
+            truncated_message=truncated_message,
+            authoritative_answer=authoritative_answer,
+            opcode=opcode,
+            response=response,
+            rescode=rescode,
+            checking_disabled=checking_disabled,
+            authed_data=authed_data,
+            z=z,
+            recursion_available=recursion_available,
+            questions=questions,
+            answers=answers,
+            authoritative_entries=auth,
+            resource_entries=res,
+        )
+
+    def __str__(self):
+        return f"DnsHeader {pprint.pformat(asdict(self))}"
+
+    def write(self, buf: Buffer):
+        buf.write("!H", self.id)
+        buf.write(
+            "!B",
+            (
+                (self.recursion_desired << 0)
+                | (self.truncated_message << 1)
+                | (self.authoritative_answer << 2)
+                | (self.opcode << 3)
+                | (self.response << 7)
+            ),
+        )
+
+        buf.write(
+            "!B",
+            (
+                (self.rescode << 0)
+                | (self.checking_disabled << 4)
+                | (self.authed_data << 5)
+                | (self.z << 6)
+                | (self.recursion_available << 7)
+            ),
+        )
+
+        buf.write(
+            "!HHHH",
+            self.questions,
+            self.answers,
+            self.authoritative_entries,
+            self.resource_entries,
+        )
+
+
+@dataclass
+class DnsQuestion:
+    name: str
+    qtype: QueryType
+
+    @classmethod
+    def parse(cls, buf: Buffer):
+        name = buf.read_qname()
+        (qtype, _) = buf.unpack("!HH")
+        return cls(name=name, qtype=QueryType(qtype))
+
+    def __str__(self):
+        return f"DnsQuestion {pprint.pformat(asdict(self))}"
+
+    def write(self, buf: Buffer):
+        buf.write_qname(self.name)
+        buf.write("!HH", self.qtype, 1)
+
+
+@dataclass
+class DnsRecordUnknown:
+    domain: str
+    qtype: int  # 16 bits
+    length: int  # 16 bits
+    ttl: int  # 32 bits
+
+    def write(self, buf: Buffer):
+        print("Writing unknown record")
+        buf.write_qname(self.domain)
+        buf.write("!HHIH", self.qtype, 1, self.ttl, self.length)
+
+
+@dataclass
+class DnsRecordA:
+    domain: str
+    addr: ipaddress.IPv4Address
+    ttl: int  # 32 bits
+
+    def write(self, buf: Buffer):
+        buf.write_qname(self.domain)
+        buf.write("!HHIH", QueryType.A, 1, self.ttl, 4)
+        buf.write("!I", int(self.addr))
+
+
+DnsRecord = Union[DnsRecordUnknown, DnsRecordA]
+
+
+def parse_dns_record(buf: Buffer) -> DnsRecord:
+    domain = buf.read_qname()
+    (qtype,) = buf.unpack("!H")
+    try:
+        qtype = QueryType(qtype)
+    except ValueError:
+        print("Unknown query type: ", qtype)
+        qtype = QueryType.UNKNOWN
+
+    _ = buf.unpack("!H")  # class
+    (ttl,) = buf.unpack("!I")
+    (length,) = buf.unpack("!H")
+
+    if qtype == QueryType.A:
+        (addr,) = buf.unpack("!I")
+        addr = ipaddress.ip_address(addr)
+        return DnsRecordA(domain=domain, addr=addr, ttl=ttl)
+
+    else:
+        return DnsRecordUnknown(domain=domain, qtype=qtype, length=length, ttl=ttl)
+
+
+@dataclass
+class DnsPacket:
+    header: DnsHeader
+    questions: List[DnsQuestion]
+    answers: List[DnsRecord]
+    authoritative_entries: List[DnsRecord]
+    resource_entries: List[DnsRecord]
+
+    @classmethod
+    def parse(cls, buf: Buffer):
+        questions = []
+        answers = []
+        authoritative_entries = []
+        resource_entries = []
+
+        header = DnsHeader.parse(buf)
+        for _ in range(header.questions):
+            questions.append(DnsQuestion.parse(buf))
+
+        for _ in range(header.answers):
+            answers.append(parse_dns_record(buf))
+
+        for _ in range(header.authoritative_entries):
+            authoritative_entries.append(parse_dns_record(buf))
+
+        for _ in range(header.resource_entries):
+            resource_entries.append(parse_dns_record(buf))
+
+        return cls(
+            header=header,
+            questions=questions,
+            answers=answers,
+            authoritative_entries=authoritative_entries,
+            resource_entries=resource_entries,
+        )
+
+    def write(self, buf: Buffer):
+        self.header.write(buf)
+        for question in self.questions:
+            question.write(buf)
+
+        for answer in self.answers:
+            answer.write(buf)
+
+        for auth in self.authoritative_entries:
+            auth.write(buf)
+
+        for resource in self.resource_entries:
+            resource.write(buf)
